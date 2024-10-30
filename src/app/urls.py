b412e6deb30df3911739f9f9ae3192566a8bc594
giscardAsan <asantegiscard23@gmail.com>
2024-10-30 22:39:57 +0000

    commit

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from  .views import CustomLoginView
+from  .views import CustomLoginView, send_welcome_email
 from .views import index
 from  .views import CustomRegisterView
 from django.contrib.auth.views import LogoutView
+from django.views.generic.base import RedirectView
 
 
 
@@ -17,9 +18,10 @@ urlpatterns = [
      path('', index, name="home"),
      path("register/",CustomRegisterView.as_view(),name="register"),
      path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
-     
-
-
+     path('sen-welcome/', send_welcome_email, name='send_welcome_email'),
+     path('accounts/login', CustomLoginView.as_view(), name='login'),
+     path('login/', RedirectView.as_view(url='/accounts/login'), name='login'),
+     path('login/', RedirectView.as_view(url='/admin'), name='login'),
 ]
 
 
