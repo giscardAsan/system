369181166a7f5638ce45514189a22763a8551609
giscardAsan <asantegiscard23@gmail.com>
2024-11-06 09:54:43 +0000

    commit

--- src/system/urls.py
+++ src/system/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
@@ -31,3 +34,4 @@ urlpatterns = [
     path("", include("product.urls")),
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
