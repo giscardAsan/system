55acd7c1469e1c5237231ce68053a3c83e288f5d
giscardAsan <asantegiscard23@gmail.com>
2024-12-08 14:38:56 +0000

    commit

--- contact/urls.py
+++ contact/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.contact, name='contact'),
+    path('action/', views.action, name='action'),
+    path('crime/', views.crime, name='crime'),
+    path('document/', views.document, name='document'),
+    path('drama/', views.drama, name='drama'),
+    path('hollywood/', views.hollywood, name='hollywood'),
+    path('horror/', views.horror, name='horror'),
+    path('kids/', views.kids, name='kids'),
+    path('recommend/', views.recommend, name='recommend'),
+    path('romantic/', views.romantic, name='romantic'),
+    path('tv_show/', views.tv_show, name='tv_show'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -5,3 +5,33 @@ from django.shortcuts import render
 
 def contact(request):
     return render(request, 'contact/contact.html')
+
+def action(request):
+    return render(request, 'contact/action.html')
+
+def crime(request):
+    return render(request, 'contact/crime.html')
+
+def document(request):
+    return render(request, 'contact/document.html')
+
+def drama(request):
+    return render(request, 'contact/drama.html')
+
+def hollywood(request):
+    return render(request, 'contact/hollywood.html')
+
+def horror(request):
+    return render(request, 'contact/horror.html')
+
+def kids(request):
+    return render(request, 'contact/kids.html')
+
+def recommend(request):
+    return render(request, 'contact/recommend.html')
+
+def romantic(request):
+    return render(request, 'contact/romantic.html')
+
+def tv_show(request):
+    return render(request, 'contact/tv_show.html')
